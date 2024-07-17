111f94e625eb5059bbd6a9092aa5c410e17cb0f4
Farhad Lafarie <farhadlafarie@gmail.com>
2024-07-17 23:58:03 +0530

    Add basename to BrowserRouter and 404 redirect

--- portfolio/src/pages/projects.js
+++ portfolio/src/pages/projects.js
@@ -1,9 +1,9 @@
-function projects() {
-  return (
-    
-<div class="  min-h-screen mt-18 p-10 pt-28 dark:bg-gray-800 dark:border-gray-700">
-<div class="grid grid-cols-4 gap-4">
-<div class="max-w-sm max-h-fit bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+function Projects() {
+    return (
+      <div className="min-h-screen mt-18 p-10 pt-28 dark:bg-gray-800 dark:border-gray-700">
+        {/* Responsive grid setup */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <div class="max-w-sm max-h-fit bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
     <a href="#">
         <img class="rounded-t-lg" src="https://i.ibb.co/MSTKtD5/Screenshot-2024-07-11-at-9-22-37-AM.png" alt="" />
     </a>
@@ -29,12 +29,6 @@ function projects() {
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">UniConnect 2024</h5>
         </a>
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">This is a ongoing project to help all type students and lecturers connect in place and share your knowlage and help each other. here what we used <b>Flutter</b></p>
-        {/* <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Read more
-             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-            </svg>
-        </a> */}
     </div>
 </div>
 <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
@@ -46,12 +40,6 @@ function projects() {
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">WhatsApp bot 2022</h5>
         </a>
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">This is a small project to create a bot for whatsapp. here what we used <b>Node.js</b></p>
-        {/* <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Read more
-             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-            </svg>
-        </a> */}
     </div>
 </div>
 <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
@@ -63,12 +51,6 @@ function projects() {
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Telegram bot 2022</h5>
         </a>
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">This is a small project to create a bot for Telegram. here what we used <b>Python</b></p>
-        {/* <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Read more
-             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-            </svg>
-        </a> */}
     </div>
 </div>
 <div class="max-w-sm max-h-fit bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
@@ -80,17 +62,11 @@ function projects() {
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">WordPress bot 2021</h5>
         </a>
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">This movie share website build using WordPress. here what we used <b>WordPress</b></p>
-        {/* <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Read more
-             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-            </svg>
-        </a> */}
     </div>
 </div>
-</div>
-</div>
-  );
-}
-
-export default projects;
+        </div>
+      </div>
+    );
+  }
+  
+  export default Projects;
