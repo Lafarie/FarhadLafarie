2ed113518b40c3b09453a58b8b4a3285f201ac36
Farhad Lafarie <farhadlafarie@gmail.com>
2024-08-14 20:44:59 +0530

    update cv to pdf view

--- portfolio/src/components/navbar.js
+++ portfolio/src/components/navbar.js
@@ -19,7 +19,7 @@ function Navbar() {
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Farhad Lafarie</span>
         </NavLink>
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-          <a href="./src/cv.pdf" download="Farhad Lafarie 11/7.pdf">
+          <a href="./src/cv.pdf" download="Farhad Lafarie August.pdf">
             <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Download CV</button>
           </a>
           <button
