522e71df997173a56ec6ce6cf99ffec423f8b8aa
farhad <farhadlafarie@gmail.com>
2025-10-17 09:04:10 +0530

    Update CV download link in navbar: change filename to remove date for consistency

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -47,7 +47,7 @@ function Navbar() {
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Farhad Lafarie</span>
         </Link>
         <div className="flex md:order-2 items-center space-x-2 ">
-          <a href="/cv.pdf" download="Farhad Lafarie August.pdf" className="hidden md:flex">
+          <a href="/cv.pdf" download="Farhad Lafarie.pdf" className="hidden md:flex">
             <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
               Download CV
             </button>
